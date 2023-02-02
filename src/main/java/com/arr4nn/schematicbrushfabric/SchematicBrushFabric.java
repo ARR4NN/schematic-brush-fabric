8fa8cbe28fab1bf4ad9d2167ef1f9418cb75337b
ARR4NN <arranhardy07@gmail.com>
2023-02-02 20:33:59 +0000

    Register keybinds and events. Send command on key click

--- src/main/java/com/arr4nn/schematicbrushfabric/SchematicBrushFabric.java
+++ src/main/java/com/arr4nn/schematicbrushfabric/SchematicBrushFabric.java
@@ -1,14 +1,9 @@
 package com.arr4nn.schematicbrushfabric;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class SchematicBrushFabric implements ModInitializer {
-    public static final String MOD_ID = "schematic-brush-fabric";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 
     @Override
     public void onInitialize() {
--- src/main/java/com/arr4nn/schematicbrushfabric/event/KeyInputHandler.java
+++ src/main/java/com/arr4nn/schematicbrushfabric/event/KeyInputHandler.java
@@ -1,48 +1,68 @@
 package com.arr4nn.schematicbrushfabric.event;
 
+import com.arr4nn.schematicbrushfabric.ToggleKeyBinding;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.Objects;
 
 public class KeyInputHandler {
     public static final String KEY_CATEGORY = "key.category.schematicbrushreborn.schematic";
     public static final String KEY_SCHEMATIC_NEXT = "key.schematicbrushreborn.schematic.next";
     public static final String KEY_SCHEMATIC_PREVIOUS = "key.schematicbrushreborn.schematic.previous";
+    public static final String KEY_SCHEMATIC_LOCK = "key.schematicbrushreborn.schematic.lock";
 
-    public static KeyBinding schematicNextKey;
-    public static KeyBinding schematicPrevKey;
+    public static ToggleKeyBinding schematicNextKey,schematicPrevKey,schematicLockKey;
 
     public static void registerKeyInputs(){
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if(schematicNextKey.wasPressed()){
-                // This happens when next key is pressed.
-                client.player.sendMessage(Text.of("next key is pressed"));
-            }
-            if(schematicPrevKey.wasPressed()){
-                // This happens when previous key is pressed.
-                client.player.sendMessage(Text.of("previous key is pressed"));
 
+            if(schematicNextKey.isToggled()) return;
+            if(schematicPrevKey.isToggled()) return;
+            if(schematicLockKey.isToggled()) return;
+
+
+            if (schematicNextKey.wasPressed()) {
+                schematicNextKey.toggle();
+                Objects.requireNonNull(client.getNetworkHandler()).sendCommand("sbr schematic next");
+            }
+            if (schematicPrevKey.wasPressed()) {
+                schematicPrevKey.toggle();
+                Objects.requireNonNull(client.getNetworkHandler()).sendCommand("sbr schematic previous");
+            }
+            if (schematicLockKey.wasPressed()) {
+                schematicLockKey.toggle();
+                Objects.requireNonNull(client.getNetworkHandler()).sendCommand("sbr schematic lock");
+                assert client.player != null;
+                client.player.playSound(SoundEvents.BLOCK_NOTE_BLOCK_PLING.value(), SoundCategory.AMBIENT,1f,2f);
             }
         });
     }
 
-    public static void register(){
-        schematicNextKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        public static void register(){
+        schematicNextKey = (ToggleKeyBinding) KeyBindingHelper.registerKeyBinding(new ToggleKeyBinding(
                 KEY_SCHEMATIC_NEXT,
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_U,
-                KEY_CATEGORY
-        ));
-
-        schematicPrevKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_SCHEMATIC_PREVIOUS,
-                InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_I,
                 KEY_CATEGORY
         ));
+
+        schematicPrevKey = (ToggleKeyBinding) KeyBindingHelper.registerKeyBinding(new ToggleKeyBinding(
+                KEY_SCHEMATIC_PREVIOUS,
+                GLFW.GLFW_KEY_U,
+                KEY_CATEGORY
+        ));
+        schematicLockKey = (ToggleKeyBinding) KeyBindingHelper.registerKeyBinding(new ToggleKeyBinding(
+                KEY_SCHEMATIC_LOCK,
+                GLFW.GLFW_KEY_J,
+                KEY_CATEGORY
+        ));
+
+
 
         registerKeyInputs();
     }
